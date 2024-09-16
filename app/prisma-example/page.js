466d4143c648aabcb0463b327c0b23f877e22f1e
UriKimhi <uri.kimhi@gmail.com>
2024-09-16 12:07:18 +0300

    vercel

--- app/prisma-example/page.js
+++ app/prisma-example/page.js
@@ -1,11 +1,12 @@
 import prisma from "@/utils/db";
 
 const prismaHandlers = async () => {
-  await prisma.task.create({
-    data: {
-      content: "eake up",
-    },
-  });
+  console.log("prisma example");
+  // await prisma.task.create({
+  //   data: {
+  //     content: "eake up",
+  //   },
+  // });
   const allTask = await prisma.task.findMany({
     orderBy: {
       createAt: "desc",
@@ -16,6 +17,11 @@ const prismaHandlers = async () => {
 
 const PrismaExample = async () => {
   const tasks = await prismaHandlers();
+
+  if (tasks.length === 0) {
+    return <h2 className="mt-8 font-medium text-lg">No tasks to show...</h2>;
+  }
+
   return (
     <div>
       <h1 className="text-5xl mb-3">prisma example</h1>
--- middleware.js
+++ middleware.js
@@ -1,7 +1,8 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
-  return NextResponse.redirect(new URL("/", request.url));
+  // return NextResponse.redirect(new URL("/", request.url));
+  return console.log("counter");
 }
 
 export const config = {
